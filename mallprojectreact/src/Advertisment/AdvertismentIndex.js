80191b0cf9089363c70c9d8126967dd2dae551a8
Deathsight <57475781+Deathsight@users.noreply.github.com>
2019-11-13 19:42:06 +0300

    NAVBAR (WIP)
    Im working on the navbar to show the user profile pictture with all the menus neeeded for searching the mall

---
+++ mallprojectreact/src/Advertisment/AdvertismentIndex.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class AdvertismentCreate extends React.Component {
+  state = {
+    User: null,
+    isCreated: false
+  };
+  render(){
+    return(
+        <h2>Hello</h2>
+    )
+
+  }
+}
