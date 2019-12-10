4994b49866408fb132d301f02af65d096ab91ed3
Deathsight <57475781+Deathsight@users.noreply.github.com>
2019-12-10 07:06:34 +0300

    i guess ?

--- mallprojectreact/src/Renting/RentingIndex.js
+++ mallprojectreact/src/Renting/RentingIndex.js
@@ -1,19 +1,82 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Table from "react-bootstrap/Table";
 import DB from "../db.js";
 import Auth from "../auth";
 import { Link } from "react-router-dom";
+//material-ui
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import {Paper,Radio,Tabs,Tab} from '@material-ui/core'
+
+import { Button } from "react-bootstrap";
+
 
 export default class RentingIndex extends React.Component {
   state = {
     Assets: [],
-    SelectedAssets: []
+    User: null,
+    SelectedAssets: [],
+    type:"Store"
+  };
+  useStyles = {
+    '@global': {
+      body: {
+        backgroundColor: 'white'
+      },
+    },
+    container:{
+        position: "relative",
+        marginLeft:"5vw",
+        paddingTop:"35px",
+        paddingBottom: "25px",
+        backgroundColor: "white",
+        boxShadow:" 25px 25px 50px",
+      },
+      background:{
+        zIndex: "2",
+        position: "relative",
+        backgroundImage:`url(https://w.wallhaven.cc/full/96/wallhaven-969w3d.jpg)`,
+        minWidth: "100vw",
+        minHeight: "94.4vh",
+        float:"left"
+      },
+    paper: {
+        paddingTop:"35px",
+        paddingRight:"30px",
+        paddingLeft:"30px",
+        paddingBottom: "25px",
+      
+    },
+    avatar: {
+      margin: 1,
+      backgroundColor: 'darkblue',
+    },
+    form: {
+      marginTop: 3,
+    },
+    submit: {
+      marginTop:4,
+      marginRight:0,
+      marginBottom:10
+    },
   };
 
+  switchType = (event, switchedType) => {
+    this.setState({accountType: switchedType})
+    console.log(switchedType)
+  }
+  handleSwitch = (type) => {
+    this.setState({type})
+  }
   async componentDidMount() {
-    const json = await DB.Assets.findByQuery("getAssets");
-    this.setState({ Assets: json });
+    const Assets = await DB.Assets.findByQuery("getAssets");
+    const User = await DB.Users.findByQuery("profile");
+    this.setState({ Assets, User });
   }
 
   handleSelect = (Id) => {
@@ -25,81 +88,102 @@ export default class RentingIndex extends React.Component {
   render() {
     return (
       <div>
-        <Link to={`/profile/complete`}>complete profile</Link>
+              <Tabs
+                value={this.state.type}
+                indicatorColor="primary"
+                textColor="primary"
+                onChange={this.switchType}
+                style={{paddingTop:10, marginLeft:"40%"}}
+                aria-label="disabled tabs example"
+                
+              >
+                <Tab label="Store"  onClick={()=> this.handleSwitch("Store")}/>
+                <Tab label="Theater" onClick={()=> this.handleSwitch("Theater")}/>
+              </Tabs>
+      {this.state.type === "Store" ?
+<Container component="main">
+        <CssBaseline />
+        <Paper style={this.useStyles.paper}>
         <h2>Stores</h2>
-
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Location Code</th>
-              <th>Description</th>
-              <th>Size</th>
-              <th>Price</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.Assets.map(item =>
-              item.Type === "Store" ? (
-                <tr key={item.Id}>
-                  <td>{item.LocationCode}</td>
-                  <td>{item.Description}</td>
-                  <td>{item.Store.Size}</td>
-                  <td>{item.Store.StorePM.Price}</td>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Location Code</TableCell>
+              <TableCell align="left">Description</TableCell>
+              <TableCell align="left">Size</TableCell>
+              <TableCell align="left">Price</TableCell>
+              <TableCell align="left"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+                {this.state.Assets ?
+                    this.state.Assets.map(item => (
+                      item.Type === "Store" ?
+                <TableRow key={item.Id}>
+                  <TableCell align="left">{item.LocationCode}</TableCell>
+                  <TableCell align="left">{item.Description}</TableCell>
+                  <TableCell align="left">{item.Store.Size}</TableCell>
+                  <TableCell align="left">{item.Store.StorePM.Price}</TableCell>
+        
+                  <TableCell align="left">
                   {Auth.isLoggedIn() && (
-                    <td>
-                      <button onClick={() => this.handleSelect(item.Id)}>Rent</button>
-                    </td>
+                    <TableCell>
+                      <Radio onClick={() => this.handleSelect(item.Id)}/>
+                    </TableCell>
                   )}
-                </tr>
-              ) : null
-            )}
-          </tbody>
+                  </TableCell>
+                  </TableRow>
+                :null)):
+                null}
+          </TableBody>
         </Table>
-        <div>
-          {this.state.SelectedAssets.map(item =>
-          <p>{item}</p>
-          )}
+        <Link to={`/renting/create/${this.state.SelectedAssets}`}><Button>Rent</Button></Link>
+      </Paper>
+      </Container>
 
-          <Link to={`/renting/create/${this.state.SelectedAssets}`}>Create</Link>
-        </div>
-        {/*Displaying the Rentable Theaters */}
-            <h1>Theaters</h1>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Location Code</th>
-              <th>Description</th>
-              <th>Size</th>
-              <th>Price</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.Assets.map(item =>
-              item.Type === "Theater" ? (
-                <tr key={item.Id}>
-                  <td>{item.LocationCode}</td>
-                  <td>{item.Description}</td>
-                  <td>{item.Theater.Size}</td>
-                  <td>{item.Theater.TheaterPM.Price}</td>
+: this.state.type === "Theater" ?
+      <Container component="main">
+        <CssBaseline />
+        
+        <Paper style={this.useStyles.paper}>
+        <h2>Theaters</h2>
+        <Table size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="left">Location Code</TableCell>
+              <TableCell align="left">Description</TableCell>
+              <TableCell align="left">Size</TableCell>
+              <TableCell align="left">Price</TableCell>
+              <TableCell align="left"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+                {this.state.Assets ?
+                    this.state.Assets.map(item => (
+                      item.Type === "Theater" ? (
+                <TableRow key={item.Id}>
+                  <TableCell align="left">{item.LocationCode}</TableCell>
+                  <TableCell align="left">{item.Description}</TableCell>
+                  <TableCell align="left">{item.Theater.Size}</TableCell>
+                  <TableCell align="left">{item.Theater.TheaterPM.Price}</TableCell>
+        
+                  <TableCell align="left">
                   {Auth.isLoggedIn() && (
-                    <td>
-                      <button onClick={() => this.handleSelect(item.Id)}>Rent</button>
-                    </td>
+                    <TableCell>
+                      <Radio onClick={() => this.handleSelect(item.Id)}/>
+                    </TableCell>
                   )}
-                </tr>
-              ) : null
-            )}
-          </tbody>
+                  </TableCell>
+                  </TableRow>
+                      ) :
+                null))
+                : null}
+          </TableBody>
         </Table>
-        <div>
-          {this.state.SelectedAssets.map(item =>
-          <p>{item}</p>
-          )}
-
-          <Link to={`/renting/theaterRenting/${this.state.SelectedAssets}`}>Create</Link>
-        </div>
+        <Link to={`/renting/theaterRenting/${this.state.SelectedAssets}`}><Button>Create</Button></Link>
+      </Paper>
+      </Container>:null
+      }
       </div>
     );
   }
